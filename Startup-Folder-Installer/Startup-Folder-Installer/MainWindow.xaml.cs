924fd3b45ad9cb6dc764f2afa05461e52f6b2981
Daniel Leonard <daniel@danleonard.us>
2014-05-16 21:13:09 -0500

    Changed some splash text stuff

--- Startup-Folder-Installer/Startup-Folder-Installer/MainWindow.xaml.cs
+++ Startup-Folder-Installer/Startup-Folder-Installer/MainWindow.xaml.cs
@@ -187,7 +187,7 @@ namespace Startup_Folder_Installer
 #endif
             }
 
-            ScrollViewer.ScrollToVerticalOffset(ScrollViewer.ScrollableHeight);
+            ScrollViewer.ScrollToBottom();
             ProgressPercentage++;
         }
 
